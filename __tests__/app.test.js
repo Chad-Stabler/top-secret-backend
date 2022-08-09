dc32baea4abe60ecaf72c0a4225030abaa65f6d8
Chad Stabler <stablerpsn@gmail.com>
2022-08-09 17:53:20 -0500

    delete route working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,24 +2,24 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'test@example.com',
   password: '12345'
 };
 
-// const regAndSignIn = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
-//   const agent = request.agent(app);
+const regAndSignIn = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
 
-//   const user = await UserService.create({ ...mockUser, ...userProps });
+  const user = await UserService.create({ ...mockUser, ...userProps });
 
-//   const { email } = user;
-//   await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
 
-//   return [agent, user];
-// };
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -33,6 +33,11 @@ describe('backend-express-template routes', () => {
       id: expect.any(String),
       email
     });
+  });
+  it('delete /sessions should delete cookie', async () => {
+    const [agent] = await regAndSignIn();
+    const resp = await agent.delete('/api/v1/users/sessions');
+    expect(resp.status).toBe(204);
   });
   afterAll(() => {
     pool.end();
