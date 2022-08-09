0ddec62fec4a806d3ddd8951f0e4f8cd1f40a97f
Chad Stabler <stablerpsn@gmail.com>
2022-08-09 17:28:36 -0500

    sign up and post route working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,38 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345'
+};
+
+// const regAndSignIn = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+//   const agent = request.agent(app);
+
+//   const user = await UserService.create({ ...mockUser, ...userProps });
+
+//   const { email } = user;
+//   await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
+
+//   return [agent, user];
+// };
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('add user route should add a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      email
+    });
   });
   afterAll(() => {
     pool.end();
