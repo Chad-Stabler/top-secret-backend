aae865931aaf6448e4358626bb352be4aa1b6282
Chad Stabler <stablerpsn@gmail.com>
2022-08-10 10:20:04 -0500

    get route for signed in user working and tested

--- __tests__/sectrets.test.js
+++ __tests__/sectrets.test.js
@@ -2,13 +2,36 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 const agent = request.agent(app);
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345'
+};
+
+const regAndSignIn = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  
+  it('/get secrets should return secrets for authenticated users', async () => {
+    const [agent] = await regAndSignIn();
+    const res = await agent.get('/api/v1/secrets');
+    console.log(res.text);
+    expect(res.status).toBe(200);
+  });
   it('/get secrets should not allow unregistered access', async () => {
     await agent.delete('/api/v1/users/sessions');
     const res = await request(app).get('/api/v1/secrets');
