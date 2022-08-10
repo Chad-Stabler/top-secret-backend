25485960695cc5e488d8800d8fd134c292b1d74f
Chad Stabler <stablerpsn@gmail.com>
2022-08-10 10:48:05 -0500

    post secret route working, tested and protected

--- __tests__/sectrets.test.js
+++ __tests__/sectrets.test.js
@@ -6,8 +6,13 @@ const UserService = require('../lib/services/UserService');
 const agent = request.agent(app);
 
 const mockUser = {
-  email: 'test@example.com',
+  email: 'test@defense.gov',
   password: '12345'
+};
+
+const testSecret = {
+  title: 'first secret test',
+  description: 'this is the test for the first secret in my database'
 };
 
 const regAndSignIn = async (userProps = {}) => {
@@ -29,7 +34,6 @@ describe('backend-express-template routes', () => {
   it('/get secrets should return secrets for authenticated users', async () => {
     const [agent] = await regAndSignIn();
     const res = await agent.get('/api/v1/secrets');
-    console.log(res.text);
     expect(res.status).toBe(200);
   });
   it('/get secrets should not allow unregistered access', async () => {
@@ -38,6 +42,19 @@ describe('backend-express-template routes', () => {
   
     expect(res.status).toBe(401);
     expect(res.body.message).toBe('You are not signed in');
+  });
+  it('posting secret should be denied for unauth users', async () => {
+    const res = await request(app).post('/api/v1/secrets').send(testSecret);
+
+    expect(res.status).toBe(401);
+  });
+  it('should post a secret for auth user', async () => {
+    const [agent] = await regAndSignIn();
+    const res = await agent.post('/api/v1/secrets').send(testSecret);
+    expect(res.status).toBe(200);
+    const { title, description } = testSecret;
+    expect(res.body.title).toBe(title);
+    expect(res.body.description).toBe(description);
   });
   afterAll(() => {
     pool.end();
